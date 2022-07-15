7cce10d9d7db9bdd08a208c64b5c1c1e8cda530b
MSagGik <108148690+MSagGik@users.noreply.github.com>
2022-07-15 10:37:38 +0300

    Update FirstServlet.java

--- src/main/java/com/example/javaeetest/FirstServlet.java
+++ src/main/java/com/example/javaeetest/FirstServlet.java
@@ -1,13 +1,21 @@
+package com.example.javaeetest;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "FirstServlet", value = "/FirstServlet")
+
 public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter pw = response.getWriter();
+
+        pw.println("<html>");
+        pw.println("<h1> Hello world! All good! </h1>");
+        pw.println("</html>");
 
     }
 
